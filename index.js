2046012367ebb2bd33b8b42df11261f8f4919c2e
Lucky Dhanda <luckydhanda21@gmail.com>
2024-10-07 14:08:05 +0530

    setup the express Router

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const express = require ('express');
 const app =express();
 const port = 8000;
 
+const router = require('./routes/index');
+
+// this will send all the requests to the index file
+app.use('/', router);
+
 app.listen(port, (err)=>{
     if(err){
         console.log(`Error occured while connecting to port ${port}`);
